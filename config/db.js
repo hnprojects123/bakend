e648c03e2e9a0cd1f077a7afcf1eab7ee717ca81
hnprojects123 <hnprojects123@gmail.com>
2025-03-13 21:36:39 +0530

    update db

--- config/db.js
+++ config/db.js
@@ -1,38 +1,51 @@
 const mysql = require("mysql");
 
-const dbConfig = {
-  host: process.env.MYSQL_HOST, // GoDaddy MySQL hostname
+// const dbConfig = {
+//   host: process.env.MYSQL_HOST, // GoDaddy MySQL hostname
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   port: 3306, // MySQL default port
+//   connectTimeout: 20000, // 20 sec timeout (increase to avoid ETIMEDOUT)
+//   acquireTimeout: 20000, // Extra timeout handling
+//   multipleStatements: true, // Allow multiple queries
+// };
+
+// let connection;
+
+// function handleDisconnect() {
+//   connection = mysql.createConnection(dbConfig);
+
+//   connection.connect((err) => {
+//     if (err) {
+//       console.error("Database connection failed:", err);
+//       setTimeout(handleDisconnect, 5000); // Retry in 5 sec if connection fails
+//     } else {
+//       console.log("✅ Connected to MySQL database!");
+//     }
+//   });
+
+//   connection.on("error", (err) => {
+//     console.error("MySQL error", err);
+//     if (err.code === "PROTOCOL_CONNECTION_LOST" || err.fatal) {
+//       console.log("🔄 Reconnecting...");
+//       handleDisconnect(); // Auto-reconnect
+//     }
+//   });
+// }
+
+// handleDisconnect();
+
+// module.exports = connection;
+const pool = mysql.createPool({
+  connectionLimit: 10, // Adjust based on your needs
+  host: process.env.MYSQL_HOST, 
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  connectTimeout: 20000, // 20 sec timeout (increase to avoid ETIMEDOUT)
-  acquireTimeout: 20000, // Extra timeout handling
-  multipleStatements: true, // Allow multiple queries
-};
-
-let connection;
-
-function handleDisconnect() {
-  connection = mysql.createConnection(dbConfig);
-
-  connection.connect((err) => {
-    if (err) {
-      console.error("Database connection failed:", err);
-      setTimeout(handleDisconnect, 5000); // Retry in 5 sec if connection fails
-    } else {
-      console.log("✅ Connected to MySQL database!");
-    }
-  });
-
-  connection.on("error", (err) => {
-    console.error("MySQL error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST" || err.fatal) {
-      console.log("🔄 Reconnecting...");
-      handleDisconnect(); // Auto-reconnect
-    }
-  });
-}
-
-handleDisconnect();
-
-module.exports = connection;
+  port: 3306,
+  waitForConnections: true,
+  queueLimit: 0
+});
+console.log("✅ Connected to MySQL database!");
+module.exports = pool;
